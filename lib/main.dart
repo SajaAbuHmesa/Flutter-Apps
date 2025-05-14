60313335d375f8183ef33671b4f68fdd00d89865
Saja Abu Hmesa <102825451+SajaAbuHmesa@users.noreply.github.com>
2025-05-14 11:05:45 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/game.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tic Tac Toe',
+      
+      home: GameScreen(),
+    );
+  }
+}
